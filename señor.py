cee0f8fe7ed7f35d1f63dd777fb0fef3893ca02c
Steven Romero <131302832+stevnromero@users.noreply.github.com>
2025-08-20 20:53:40 -0500

    Add files via upload

---
+++ señor.py
@@ -0,0 +1,79 @@
+
+
+
+""""
+def name(nombre, apellido):
+    return f"{nombre} {apellido}, eres divina"
+
+
+
+def multiplicacion(x):
+    return 5 * x
+
+
+def suma(x):
+    return 5 + x
+
+
+
+def family(parentesco1 = "mamá"):
+    print("hola " + parentesco1)
+
+
+family("abuela")
+family("papá")
+family("tía")
+family("hijo")
+family()
+
+
+
+def cantante(nombre1, nombre2, nombre3, *args):
+    print("quien es la banda mas popular: " + nombre1)
+    print("cual es tu banda favorita: " + nombre2)
+    print("quien tiene mas seguidores: " + nombre3) 
+
+    for cv in args:
+        print("nombre de la cancion: " + cv)
+
+
+
+
+def party(edad):
+    edad = int(edad)
+
+    if edad >= 18:
+        print("puedes pasar")
+    elif edad == 17:
+        print("entras mínimo a los 17 años")
+    else:
+        print("no puedes entrar")
+
+    return edad
+
+
+
+def emplooyee(name):
+    print("hola " + "soy " + name)
+
+
+
+def employee2(fname,sname):
+    print("buenos dias " + "sr " + fname + " " + sname)
+"""
+
+
+"""def amistad(x):
+    print(f"Hola {x}, eres una persona impresionante!.")
+
+def compartir_dulce(nombre, dulce):
+    for _ in dulce:  # el contenido no importa, solo para mostrar 1 vez
+        print(f"{nombre}, compartamos {dulce}?")
+        break
+
+def responder(respuesta):
+    if respuesta.lower() == "si":
+        print("claro que sí")
+    else:
+        print("Lo siento mucho")
+"""
